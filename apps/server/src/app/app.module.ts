2a0e9a182b630b1868062934b774db002dcd9419
Zebzeev Artemiy <pride.ots@gmail.com>
2024-03-13 12:53:33 +0300

    [sav-1] Add mongoose module (#3)
    Co-authored-by: Artemiy Zebzeev <a.zebzeev@keyauto.ru>

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AccountModule } from './account/account.module';
 import { AppService } from './app.service';
 
+import 'dotenv/config';
+
 @Module({
-  imports: [AccountModule],
+  imports: [
+    AccountModule,
+    MongooseModule.forRoot(
+      `${process.env.MONGO_CONNECTION_URL}/${process.env.DB_NAME}`
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
